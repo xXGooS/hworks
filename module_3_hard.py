e320655c47519869974f08c3d9093667c1fe90c8
xXGooS <wjw1256838614@gmail.com>
2024-10-20 00:09:33 +0300

    Add files via upload

---
+++ module_3_hard.py
@@ -0,0 +1,29 @@
+def calculate_structure_sum(*args):
+    summa = 0
+
+    for data in args:
+        if isinstance(data, (int, float)):
+            summa += data
+        elif isinstance(data, str):
+            summa += len(data)
+        elif isinstance(data, (list, tuple, set)):
+            summa += calculate_structure_sum(*data)
+        elif isinstance(data, dict):
+            for key, value in data.items():
+                summa += calculate_structure_sum(key)
+                summa += calculate_structure_sum(value)
+
+    return summa
+
+
+# Пример использования
+data_structure = [
+    [1, 2, 3],
+    {'a': 4, 'b': 5},
+    (6, {'cube': 7, 'drum': 8}),
+    "Hello",
+    ((), [{(2, 'Urban', ('Urban2', 35))}])
+]
+
+result = calculate_structure_sum(data_structure)
+print(result)  #99
