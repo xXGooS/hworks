df8194273bf073fdb71862e5598f1e898f57c2ac
xXGooS <wjw1256838614@gmail.com>
2024-11-24 01:58:38 +0300

    Update module_9_1.py

--- module_9_1.py
+++ module_9_1.py
@@ -4,5 +4,7 @@ def apply_all_func(int_list, *functions):
         result[function.__name__] = function(int_list)
     return result
 
+# Пример результата выполнения программы:
+
 print(apply_all_func([6, 20, 15, 9], max, min))
 print(apply_all_func([6, 20, 15, 9], len, sum, sorted))
